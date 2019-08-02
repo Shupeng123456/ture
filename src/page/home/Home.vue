f10235addc4d789c13b63f9dba96b1dca09afde5
[shu123456] <[1050378320@qq.com]>
2019-08-02 21:40:57 +0800

    init

---
+++ src/page/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="text">home<home-Header></home-Header></div>
+</template>
+
+<script>
+  import homeHeader from './components/homeHeader'
+	export default{
+		name:'Home',
+		components:{
+			homeHeader
+		}
+	}
+</script>
+
+<style lang='stylus'>
+.text
+  color:blue;
+</style>
